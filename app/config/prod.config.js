fa0810cb07498c3ba508d02bbf2e39d12704e5db
Genet2327 <genetaddisalem@gmail.com>
2022-10-12 00:17:37 -0500

    change

---
+++ app/config/prod.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    HOST: 'localhost',
+    port: 3306,
+    USER: 'admin',
+    PASSWORD: 'cs@oc2022t6',
+    DB: 'p2-t6-schedule',
+    dialect: 'mysql',
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
+    },
+  };
