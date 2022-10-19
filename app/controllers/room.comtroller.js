2d6e67bc3c95787b6b6807866021f03924fbbf15
Shawn Turner <shawn.m.turner@eagles.oc.edu>
2022-10-18 23:31:27 -0500

    update room controller and models

---
+++ app/controllers/room.comtroller.js
@@ -0,0 +1,113 @@
+const db = require("../models");
+const { Op } = require("sequelize");
+const Room = db.room;
+
+// Create and Save a new Tutorial
+exports.create = (req, res) => {
+  // Validate request
+  if (!req.body.number) {
+    res.status(400).send({
+      message: "Number can not be empty!"
+    });
+    return;
+  } else if (!req.body.capacity) {
+    res.status(400).send({
+      message: "Capacity can not be empty!"
+    });
+    return;
+  }
+  
+  const room = {
+    number: req.body.number,
+    capacity: req.body.capacity
+  };
+
+  // Create and Save a new Course
+  Room.create(room)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creating the Course."
+      });
+    });
+};
+
+// Retrieve all Courses from the database
+exports.findAll = (req, res) => {
+  Room.findAll()
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving Courses."
+      });
+    });
+};
+
+// Retrieve a single Course with an id
+exports.findById = (req, res) => {
+  const id = req.params.id;
+  Room.findByPk(id)
+    .then(data => {
+      if (data) {
+        res.send(data);
+      } else {
+        res.status(404).send({
+          message: 'Cannot find Course with id=' + id
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: 'Error retrieving Course with id=' + id
+      });
+    });
+};
+
+// Update a Course by the id in the request
+exports.update = (req, res) => {
+  const id = req.params.id;
+  Room.update(req.body, {
+    where: { id: id }
+  })
+  .then(num => {
+    if (num == 1) {
+      res.send({
+        message: 'Course was updated successfully.'
+      });
+    } else {
+      res.send({
+        message: 'Cannot update Course with id=' + id + '. Maybe Course was not found or req.body is empty!'
+      });
+    }
+  })
+  .catch(err => {
+    res.status(500).send({
+      message: 'Error updating Course with id=' + id
+    });
+  });
+};
+
+// Delete a Course with the specified id in the request
+exports.delete = (req, res) => {
+  const id = req.params.id;
+  Room.destroy({
+    where: { id: id }
+  })
+  .then(num => {
+    if (num == 1) {
+      res.send({
+        message: 'Course was deleted successfully!'
+      });
+    } else {
+      res.send({
+        message: 'Cannot delete Course with id=${id}. Maybe Course was not found or '
+      })
+    }
+  })
+};
--- app/models/room.model.js
+++ app/models/room.model.js
@@ -1,13 +1,22 @@
 module.exports = (sequelize, Sequelize) => {
-    const Room = sequelize.define("room", {
-      
-      number: {
-        type: Sequelize.STRING
-      },
-      level: {
-        type: Sequelize.INTEGER(11)
-      }
-    });
-  
-    return Room;
-  };
+  const Room = sequelize.define("room", {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      unique: true
+    },    
+    number: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },    
+    capacity: {
+      type: Sequelize.INTEGER
+    },
+  },
+  {
+    timestamps: false
+  });
+
+  return Room;
+};
