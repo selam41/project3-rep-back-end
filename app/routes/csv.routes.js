38a46dfbfb366541e066a314b506ff3db14469bf
XVIIPharaoh <99553090+XVIIPharaoh@users.noreply.github.com>
2022-11-16 03:44:12 -0600

    CSV stuff

---
+++ app/routes/csv.routes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const csvController = require("../controllers/tutorials/csv.controller");
+const upload = require("../middlewares/upload");
+
+let routes = (app) => {
+  router.post("/upload", upload.single("file"), csvController.upload);
+  router.get("/tutorials", csvController.getTutorials);
+
+  app.use("/api/csv", router);
+};
+
+module.exports = routes;
